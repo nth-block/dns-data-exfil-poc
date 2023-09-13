dac58c5933c426dc67a9b2209d8977ad13dd3e52
nth-block <20584285+nth-block@users.noreply.github.com>
2023-09-13 15:39:40 +0530

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -3,4 +3,4 @@ rm victim/an-ip-file.txt victim/encoded victim/pass.tar.gz victim/xaa*
 rm attacker/an-ip-file.txt attacker/reconstruction attacker/reconstructed.tar.gz
 
 # In case you are a git client God...
-git revert
+git clean -f -d
